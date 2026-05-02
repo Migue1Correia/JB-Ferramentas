9c6418bc0451cc7e0be48a755b7fcd1b1621f102
Allan Ferreira <afsn01@protonmail.com>
2026-05-02 14:18:49 -0300

    add PersonModel class

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
-from model.db import jb_solucoes_db
-from model.auth import auth, person_create, person_get, user_create, person_exist, user_update
+from flask_bcrypt import Bcrypt
 
+from model.db import jb_solucoes_db, jb_bcrypt
+
+from model.user_account import UserAccountModel
+from model.person import PersonModel
 
 
 app = Flask(__name__)
@@ -10,7 +13,9 @@ app.config['MYSQL_HOST']        = 'Seu host' #Geralmente é localhost
 app.config['MYSQL_USER']        = 'Seu usuário para acessar o MySQL'
 app.config['MYSQL_PASSWORD']    = 'Sua senha para acessar o MySQL' # Ou deixa vazio se não tiver configurado senha
 app.config['MYSQL_DB']          = 'O nome do seu banco'
+
 jb_solucoes_db.init_app(app)
+jb_bcrypt = Bcrypt(app)
 
 # Depois configurar um arquivo .env para armazenar os dados de acesso ao banco
 
@@ -25,8 +30,10 @@ def login():
 
     elif request.method == "POST":
         status = "Usuário autenticado"
-        if not auth(request.form['usuario'], request.form['senha']):
+
+        if not UserAccountModel.auth(request.form['usuario'], request.form['senha']):
             status = "Erro ao autenticar usuário."
+        UserAccountModel.update("alex", "Alex123")
         return render_template('auth.html', status=status)
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -44,23 +51,25 @@ def register():
         phone_number    = request.form['telefone']
 
         user            = request.form['usuario']
-        # PS: Gerar e armazenar apenas o HASH da senha depois
         password        = request.form['senha']
 
-        if person_exist(code, by="code"):
+        if PersonModel.exist(code, by="code"):
             status = "Já tem uma pessoa cadastrado com esse código de CPF/CNPJ. For favor, faça o login."
             return render_template('register.html', status=status)
 
-        if not person_create(name, type, code, address, email, phone_number):
+        if not PersonModel.create(name, type, code, address, email, phone_number):
             status = "Não foi possível fazer o cadastro"
             return render_template('register.html', status=status)
 
-        person_infos = person_get(code)
+        person_infos = PersonModel.get(code)
+
         if person_infos is None:
             status = "Não foi possível extrair o código da pessoa"
             return render_template('register.html', status=status)
 
-        if not user_create(user, password, person_infos["id"], 1, True):
+        hashed_password = jb_bcrypt.generate_password_hash(password).decode("utf-8")
+
+        if not UserAccountModel.create(user, hashed_password, person_infos["id"], 1, True):
             status = f"Não foi possível gerar uma conta usuário para o {person_infos[1]}. Por favor, tente novamente!"
 
         return render_template('register.html', status=status)
